492351842b485b5791b3aca272137e1937b6caef
Amirol Ahmad <dev.amirol@gmail.com>
2021-06-04 20:37:00 +0800

    fixed the total payment

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,8 +6,8 @@ class Admin::DashboardController < AdminController
     @unpaid_orders = Order.where('paid= ?', false)
     @today_orders = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     @pending_orders = Order.where('status != ?', 3)
-    @total_weekly_paid_orders = Order.where(created_at: date_beginning_this_week..Time.zone.now).sum(:total_price)
-    @total_monthly_paid_orders = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).sum(:total_price)
+    @total_weekly_paid_orders = Order.where(created_at: date_beginning_this_week..Time.zone.now).where('paid= ?', true).sum(:total_price)
+    @total_monthly_paid_orders = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).where('paid= ?', true).sum(:total_price)
     @total_orders_this_month = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
   end
 end
