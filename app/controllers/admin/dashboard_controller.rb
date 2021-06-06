957635e2529d5cb86be9a3ac8e839a0db8105560
Amirol Ahmad <dev.amirol@gmail.com>
2021-06-06 09:07:31 +0800

    add note

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,7 @@ class Admin::DashboardController < AdminController
     @paid_orders = Order.where('paid= ?', true)
     @unpaid_orders = Order.where('paid= ?', false)
     @today_orders = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-    @pending_orders = Order.where('status != ?', 3)
+    @pending_orders = Order.where('status != ?', 3).where('paid= ?', true)
     @total_weekly_paid_orders = Order.where(created_at: date_beginning_this_week..Time.zone.now).where('paid= ?', true).sum(:total_price)
     @total_monthly_paid_orders = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).where('paid= ?', true).sum(:total_price)
     @total_orders_this_month = Order.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
